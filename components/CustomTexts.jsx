065e72a1cbf80ad19a4222602766b4c4dd4f755d
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-07-01 20:33:53 +0700

    reformatting CustomText.jsx

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import { textContainer, textVariant2 } from "../utils/motion";
+import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = ({title, textStyles}) => (
+export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
@@ -16,9 +16,9 @@ export const TypingText = ({title, textStyles}) => (
     ))}
 
   </motion.p>
-  );
+);
 
-export const TitleText = ({title, textStyles}) => (
+export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
